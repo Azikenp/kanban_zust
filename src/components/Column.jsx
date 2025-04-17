21bdeefbf1257f6453f92cb935bc41824c02c838
Azikenp <azikenp26@gmail.com>
2025-04-17 18:43:59 +0100

    yes

--- src/components/Column.jsx
+++ src/components/Column.jsx
@@ -1,16 +1,20 @@
-import { useMemo } from "react";
+import { useMemo, useState } from "react";
 import { useStore } from "../store";
 import "./Column.css";
 import Task from "./Task";
 
 const Column = ({ state }) => {
+  const [text, setText] = useState("");
+  const [open, setOpen] = useState(false);
+
   const tasks = useStore((store) => store.tasks);
-  const addTask = useStore((store) => store.addTask);
 
   const filteredTasks = useMemo(
     () => tasks.filter((task) => task.state === state),
     [tasks, state]
   );
+
+  const addTask = useStore((store) => store.addTask);
 
   return (
     <div className="column">
@@ -18,7 +22,7 @@ const Column = ({ state }) => {
         <p>{state}</p>
         <button
           onClick={() => {
-            addTask("hello" + state, state);
+            setOpen((prev) => !prev);
           }}
         >
           Add
@@ -28,6 +32,23 @@ const Column = ({ state }) => {
       {filteredTasks.map((task, index) => (
         <Task key={index} title={task.title} state={task.state} />
       ))}
+
+      {open && (
+        <div className="Modal">
+          <div className="modalContent">
+            <input onChange={(e) => setText(e.target.value)} value={text} />
+            <button
+              onClick={() => {
+                addTask(text, state);
+                setText("");
+                setOpen(false);
+              }}
+            >
+              Submit
+            </button>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
